227abfe375ff1969c0f2c1c16a712f5ab67d3cb6
ozaykina <137483443+ozaykina@users.noreply.github.com>
2023-06-25 20:07:47 +0300

    Update run_init_db.sh

--- bash/run_init_db.sh
+++ bash/run_init_db.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+docker run --name mydocker -e POSTGRES_PASSWORD="@sde_password012" -e POSTGRES_USER=test_sde -e POSTGRES_DB=demo -d -p 5432:5432 -v $(pwd)/sql:/var/lib/postgresql/sql postgres
+sleep 5
+docker exec -it mydocker psql -d demo -U test_sde -f /var/lib/postgresql/sql/init_db/demo.sql
